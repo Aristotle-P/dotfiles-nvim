f39b27b7b4e22305d18753447391e92551a694c5
= <aristotlepoultney@gmail.com>
2023-07-13 21:54:01 -0400

    updated config

--- init.lua
+++ init.lua
@@ -15,6 +15,22 @@ require("lazy").setup("plugins")
 require("config.keymaps")
 require("config.autocmds")
 local lsp = require('lsp-zero').preset({})
+local cmp = require('cmp')
+
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,8 @@
-vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-	callback = function()
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none"}) 
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+  pattern = {"*"},
+  callback = function()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  end,
 })
+
