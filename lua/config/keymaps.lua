2444bc83fd02839ce1d18335ff7172bd18e6cbea
= <aristotlepoultney@gmail.com>
2023-07-17 23:11:57 -0400

    added harpoon + config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,16 @@
 local builtin = require('telescope.builtin')
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>hi", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(4) end)
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<leader>gs', function()
@@ -7,27 +19,26 @@ end)
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move highlighted text
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Keep cursor centered when half-screen jumping or searching
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- Paste while keeping current buffer text
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+-- Yank and delete to system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
-vim.keymap.set("n", "Q", "<nop>")
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
+-- Find and replace
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,8 @@ vim.g.maplocalleader = " "
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.opt.guicursor = ""
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -17,6 +19,7 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
+-- vim.opt.scroll = 10 
 vim.opt.scrolloff = 10 
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
