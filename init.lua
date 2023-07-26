c0f19dd0613e10bdc70c24dfab6537620b92a5a5
= <aristotlepoultney@gmail.com>
2023-07-25 21:55:36 -0400

    keymaps update

--- init.lua
+++ init.lua
@@ -19,8 +19,8 @@ local cmp = require('cmp')
 
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
@@ -33,7 +33,7 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({buffer = bufnr})
 end)
 
 lsp.setup()
